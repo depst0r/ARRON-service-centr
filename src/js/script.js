1a8ebf1e64d17ae357969e14fc790430b5583881
depstor <depstor90@gmail.com>
2025-06-24 16:02:21 +0300

    header-v2

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,18 @@ import '/src/sass/style.scss';
 const burger = document.querySelector('.header__menu-wrapper ');
 const close = document.querySelectorAll('.gamburger__line');
 const menu = document.querySelector('.header__menu');
+const overlay = document.querySelector('.overlay');
 
 const gamburgerMenu = (btnOpen, btnClose, burger) => {
     const active = 'header__menu-wrapper_active'
     btnOpen.addEventListener('click', () => {
         burger.classList.add(active)
-        // document.body.style.overflow = 'hidden'
+        overlay.classList.add('overlay_active')
         btnClose.forEach(element => {
             if (element.classList.contains('gamburger__line_close')) {
                 element.classList.remove('gamburger__line_close')
                 burger.classList.remove(active)
+                overlay.classList.remove('overlay_active')
             } else {
                 element.classList.add('gamburger__line_close')
             }
