60134ba85286028daab140625fd2bb7bfa0f518b
depstor <depstor90@gmail.com>
2025-06-24 12:13:53 +0300

    burger-menu-animation-v2

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 import '/src/sass/style.scss';
+
+// Burger menu
+
+const burger = document.querySelector('.header__menu-wrapper ');
+const close = document.querySelectorAll('.gamburger__line');
+const menu = document.querySelector('.header__menu');
+
+const gamburgerMenu = (btnOpen, btnClose, burger) => {
+    const active = 'header__menu-wrapper_active'
+    btnOpen.addEventListener('click', () => {
+        burger.classList.add(active)
+        // document.body.style.overflow = 'hidden'
+        btnClose.forEach(element => {
+            if (element.classList.contains('gamburger__line_close')) {
+                element.classList.remove('gamburger__line_close')
+                burger.classList.remove(active)
+            } else {
+                element.classList.add('gamburger__line_close')
+            }
+        });
+
+    })
+}
+
+gamburgerMenu(menu, close, burger)
